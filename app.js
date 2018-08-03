12cb75897b76d34d83ae18ee85148d475f9181f9
Irene Hakes <irenebrbr@gmail.com>
2018-08-03 15:28:59 -0400

    did a whole lot of stuff

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 const express = require('express');
 const morgan = require('morgan');
+const layout = require('./views/layout');
+const models = require('./models');
+const wikiRouter = require('./routes/wiki');
+const userRouter = require('./routes/user');
+
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
+app.use('/wiki', wikiRouter);
+app.use('/user', userRouter);
 
-app.get('/', (req, res) => {
-  res.end('it works!');
+models.db.authenticate().
+then(() => {
+  console.log('connected to the database');
 })
 
 const PORT = 1337;
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+
+const init = async () => {
+  await models.db.sync({force: true});
+
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  })
+}
+
+init();
+
+app.get('/', (req, res) => {
+  res.redirect('/wiki');
 })
+
+
+
